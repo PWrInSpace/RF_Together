a5d765b0b1687fc9d4b61a909c289b53ce5deb72
juleczkatruskaweczka <julka20020@gmail.com>
2025-07-20 12:40:45 +0200

    add data protocol

---
+++ components/data_protocol/data_protocol.c
@@ -0,0 +1,20 @@
+#include "data_protocol.h"
+
+/**
+ * @brief 
+ */
+uint16_t data_protocol_calculate_crc16(const uint8_t *data, size_t length) {
+    uint16_t crc = 0xFFFF; 
+
+    for (size_t i = 0; i < length; i++) {
+        crc ^= (uint16_t)data[i] << 8;
+        for (uint8_t j = 0; j < 8; j++) {
+            if (crc & 0x8000) {
+                crc = (crc << 1) ^ 0x1021; // wielomian 0x1021
+            } else {
+                crc <<= 1;
+            }
+        }
+    }
+    return crc;
+}
---
+++ components/data_protocol/data_protocol.h
@@ -0,0 +1,43 @@
+#ifndef DATA_PROTOCOL_H
+#define DATA_PROTOCOL_H
+
+#include <stdint.h>
+
+// dodac typy pakietow!
+typedef enum {
+    PACKET_TYPE_TELEMETRY = 0x01,
+    PACKET_TYPE_COMMAND = 0x02,
+    PACKET_TYPE_ACK = 0x03,
+} packet_type_t;
+
+
+#pragma pack(1)
+typedef struct {
+    uint8_t packet_type;
+    uint32_t timestamp_ms;
+    
+    float latitude;
+    float longitude;
+    float altitude;
+    uint8_t satellites;
+    // dane z mcb - dodac reszte
+    float pressure;
+    float temperature;
+    float acceleration_x;
+
+    uint16_t status_flags;
+    uint16_t crc16;
+} telemetry_frame_t;
+#pragma pack()
+
+
+#pragma pack(1)
+typedef struct {
+    uint8_t packet_type;
+    uint8_t command_id;
+    uint32_t command_arg;
+    uint16_t crc16;
+} command_frame_t;
+#pragma pack()
+
+#endif 
